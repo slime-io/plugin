aa7d2bf0f7658c6d4233424f338f96368462fa2f
fangzhiheng <fangzhiheng@corp.netease.com>
2021-10-16 00:00:24 +0800

    Adapt to bundle mode and new config format

--- module/module.go
+++ module/module.go
@@ -3,12 +3,14 @@ package module
 import (
 	"os"
 
+	"github.com/golang/protobuf/proto"
 	log "github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	pluginapiv1alpha1 "slime.io/slime/modules/plugin/api/v1alpha1"
 	"slime.io/slime/modules/plugin/controllers"
+	"slime.io/slime/slime-framework/apis/config/v1alpha1"
 	istionetworkingapi "slime.io/slime/slime-framework/apis/networking/v1alpha3"
 	"slime.io/slime/slime-framework/bootstrap"
 	"slime.io/slime/slime-framework/model"
@@ -17,10 +19,15 @@ import (
 const Name = "plugin"
 
 type Module struct {
+	config v1alpha1.Plugin
 }
 
 func (m *Module) Name() string {
 	return Name
+}
+
+func (m *Module) Config() proto.Message {
+	return &m.config
 }
 
 func (m *Module) InitScheme(scheme *runtime.Scheme) error {
@@ -37,6 +44,12 @@ func (m *Module) InitScheme(scheme *runtime.Scheme) error {
 }
 
 func (m *Module) InitManager(mgr manager.Manager, env bootstrap.Environment, cbs model.ModuleInitCallbacks) error {
+	cfg := &m.config
+	if env.Config != nil && env.Config.Plugin != nil {
+		cfg = env.Config.Plugin
+	}
+	_ = cfg // unused until now
+
 	var err error
 	if err = (&controllers.PluginManagerReconciler{
 		Client: mgr.GetClient(),
